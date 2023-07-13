4aae23172750681bdfa2e349ed3a375adc4be5ec
Victoria Fuceneco <fuceneco.vi@gmail.com>
2023-07-12 21:30:44 -0300

    File management challenge

--- index.js
+++ index.js
@@ -1,39 +1,55 @@
+const fs = require('fs');
+
 class ProductManager {
-  constructor() {
+  constructor(path) {
     this.products = [];
     this.id = 1;
-  };
+    this.path = path;
+    this.loadProducts();
+  }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
+  loadProducts() {
+    try {
+      const data = fs.readFileSync(this.path, 'UTF-8');
+      if (data) {
+        this.products = JSON.parse(data);
+        this.id = this.products.length + 1;
+      }
+    } catch (error) {
+      console.log('Error cargando productos:', error.message);
+    }
+  }
+
+  saveProducts() {
+    try {
+      fs.writeFileSync(this.path, JSON.stringify(this.products, null, 2), 'UTF-8');
+    } catch (error) {
+      console.log('Error guardando productos:', error.message);
+    }
+  }
+
+  addProduct(product) {
+    if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
       console.error('Todos los campos son obligatorios');
       return;
     }
-
-    const existingProduct = this.products.find((product) => product.code === code);
-
+  
+    const existingProduct = this.products.find((p) => p.code === product.code);
+  
     if (existingProduct) {
       console.error('Ya existe un producto con el mismo código');
-      return
-    };
-
-    const newProduct = {
-      id: this.id,
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock
-    };
-
+      return;
+    }
+  
+    product.id = this.id;
     this.id++;
-    this.products.push(newProduct);
-  };
+    this.products.push(product);
+    this.saveProducts();
+  }
 
   getProducts() {
-   return this.products;
-  };
+    return this.products;
+  }
 
   getProductById(id) {
     const product = this.products.find((product) => product.id === id);
@@ -42,28 +58,77 @@ class ProductManager {
     } else {
       console.error('Not found');
     }
-  };
+  }
+
+  updateProduct(id, updatedFields) {
+    const productIndex = this.products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      const updatedProduct = { ...this.products[productIndex], ...updatedFields };
+      this.products[productIndex] = updatedProduct;
+      this.saveProducts();
+    } else {
+      console.error('Producto no encontrado');
+    }
+  }
+
+  deleteProduct(id) {
+    const productIndex = this.products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      this.products.splice(productIndex, 1);
+      this.saveProducts();
+    } else {
+      console.error('Producto no encontrado');
+    }
+  }
 }
 
-const manager = new ProductManager();
+module.exports = ProductManager;
 
-manager.addProduct(
-  'Producto 0',
-  'Descripción',
-  '$1500',
-  'imagen.jpg',
-  'P000',
-  200,
-);
+const manager = new ProductManager('products.json');
 
-manager.addProduct(
-  'Producto 1',
-  'Descripción',
-  '$2700',
-  'imagen.jpg',
-  'P001',
-  48,
-);
+manager.addProduct({
+  title: 'Producto prueba',
+  description: 'Este es un producto prueba',
+  price: '$200',
+  thumbnail: 'Sin imagen',
+  code: 'abc123',
+  stock: 25,
+});
 
-console.log(manager.getProducts());
-console.log(manager.getProductById(1));
+console.log('Los productos son: ', manager.getProducts());
+
+manager.addProduct({
+  title: 'Segundo producto prueba',
+  description: 'Este es el segundo producto prueba',
+  price: '$500',
+  thumbnail: 'Sin imagen',
+  code: 'abc456',
+  stock: 48,
+});
+
+console.log('Los productos parte 2 son: ', manager.getProducts());
+
+try {
+  const id = 1;
+  const product = manager.getProductById(id);
+  console.log(`El producto de id ${id} es: `, product);
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const id = 1;
+  manager.updateProduct(id, { price: '$500' });
+  const updatedProduct = manager.getProductById(id);
+  console.log('El producto actualizado es: ', updatedProduct);
+} catch (error) {
+  console.error(error.message);
+}
+
+try {
+  const id = 1;
+  manager.deleteProduct(id);
+  console.log('El listado de productos actualizado es: ', manager.getProducts());
+} catch (error) {
+  console.error(error.message);
+}
